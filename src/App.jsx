1b01ce02070d5fdf5b8d4b8ca99e60efd48e3599
Justin Marin <jumarin@protonmail.com>
2021-06-09 16:14:11 -0400

    Added useState and useEffect to load Pet array from initial GET

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
+// @ts-nocheck
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Link, Route, Switch } from 'react-router-dom'
 
 export function App() {
+  const [petResults, setPetResults] = useState([])
+
+  useEffect(async () => {
+    const response = await axios.get(
+      `https://tamagotchi-justin.herokuapp.com/api/Pets?input=""`
+    )
+    setPetResults(response.data)
+  }, [])
+
   return (
     <>
       <header>
-        <h1>Welcome to my SPA</h1>
+        <h1>Tamagotchi</h1>
         <nav>
           <ul>
             <li>
               <Link to="/">Go Home</Link>
             </li>
             <li>
-              <Link to="/1">Page 1</Link>
+              <Link to="/1">Create New Pet</Link>
             </li>
             <li>
               <Link to="/2">Page 2</Link>
@@ -22,10 +33,23 @@ export function App() {
       </header>
       <Switch>
         <Route exact path="/">
-          Home
+          <div>
+            {petResults.map(function (pet) {
+              return (
+                <article key={pet.id}>
+                  <h3>{pet.name}</h3>
+                  <section>
+                    <p>{pet.birthday}</p>
+                    <p>{pet.hungerLevel}</p>
+                    <p>{pet.happinessLevel}</p>
+                  </section>
+                </article>
+              )
+            })}
+          </div>
         </Route>
         <Route exact path="/1">
-          Page 1
+          What is your new pet's name?
         </Route>
         <Route exact path="/2">
           Page 2
