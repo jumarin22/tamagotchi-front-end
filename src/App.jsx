cbb33c47a5fe7cfbdd57f0e396c3e0842fc1e7d4
Justin Marin <jumarin@protonmail.com>
2021-06-10 16:42:21 -0400

    Added single pet item navigation and delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 // @ts-nocheck
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { Link, Route, Switch } from 'react-router-dom'
+import { Link, Route, Switch, useHistory, useParams } from 'react-router-dom'
 import { ShowPetList } from './components/ShowPetList'
 
 export function App() {
+  const history = useHistory()
+  let isRedirect = true
   const [petResults, setPetResults] = useState([])
   const [newPetName, setNewPetName] = useState('')
 
@@ -32,23 +34,70 @@ export function App() {
         setPetResults(newPets)
       }
       alert(`Created new pet ${newPetName}!`)
+      history.push('/')
     }
+  }
+
+  function PetPage() {
+    const [petItem, setPetItem] = useState({
+      id: undefined,
+      name: '',
+      birthday: '',
+      hungerLevel: undefined,
+      happinessLevel: undefined,
+      lastInteractedWithDate: '',
+      isDead: false,
+    })
+    const params = useParams()
+
+    useEffect(function () {
+      async function loadOnePet() {
+        const response = await axios.get(
+          `https://tamagotchi-justin.herokuapp.com/api/Pets/${params.id}`
+        )
+
+        if (response.status === 200) {
+          setPetItem(response.data)
+        }
+      }
+
+      loadOnePet()
+    }, [])
+
+    async function deletePet() {
+      const response = await axios.delete(
+        `https://tamagotchi-justin.herokuapp.com/api/Pets/${params.id}`
+      )
+      if (response.status === 204) {
+        alert('Pet Deleted!')
+        history.push('/')
+      }
+    }
+
+    return (
+      <article key={petItem.id}>
+        <h3>{petItem.name}</h3>
+        <section>
+          <p>{petItem.birthday}</p>
+          <p>{petItem.hungerLevel}</p>
+          <p>{petItem.happinessLevel}</p>
+          <button onClick={deletePet}>Delete Pet</button>
+        </section>
+      </article>
+    )
   }
 
   return (
     <>
       <header>
-        <h1>Tamagotchi</h1>
+        <h1>Petagotchi</h1>
         <nav>
           <ul>
             <li>
               <Link to="/">See All Pets</Link>
             </li>
             <li>
-              <Link to="/1">Create New Pet</Link>
-            </li>
-            <li>
-              <Link to="/2">Page 2</Link>
+              <Link to="/create">Create New Pet</Link>
             </li>
           </ul>
         </nav>
@@ -59,7 +108,7 @@ export function App() {
             <ShowPetList pet={petResults} />
           </div>
         </Route>
-        <Route exact path="/1">
+        <Route exact path="/create">
           <h4>What is your new pet's name?</h4>
           <form onSubmit={handleCreateNewPet}>
             <input
@@ -71,8 +120,8 @@ export function App() {
             />
           </form>
         </Route>
-        <Route exact path="/2">
-          Page 2
+        <Route path="/:id">
+          <PetPage />
         </Route>
         <Route path="*">Not Found</Route>
       </Switch>
